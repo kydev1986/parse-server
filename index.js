2549c202051a0fd2f4e982dd63480972f9c2912b
source_server <source_server@example.com>
2016-06-08 13:24:06 +0000

    modify workspace

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var databaseUri = 'mongo;
+var databaseUri = 'mongodb://;
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');

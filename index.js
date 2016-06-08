563277dca681eff71c7bffcb12438dacd537162a
source_server <source_server@example.com>
2016-06-08 13:25:52 +0000

    modify workspace

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ var databaseUri = 'mongodb://adminkuku:kuku1688@ds051953.mlab.com:51953/testkuku
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'm',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'app',
+  masterKey: process.env.MASTER_KEY || 'k', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
